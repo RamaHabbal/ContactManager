fe0fcac9003f4e7591f83a4620c5806739758959
RamaHabbal <137204591+RamaHabbal@users.noreply.github.com>
2023-08-05 11:57:55 +0300

    added backend folder and activate JWT

---
+++ ContactManagerBackend/routes/api.php
@@ -0,0 +1,14 @@
+<<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+    
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();});
+});
